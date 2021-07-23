4331ee46147c56863a1d42cbd4dd080c766f885b
ipmstyle <ipmstyle@gmail.com>
2021-07-23 15:37:09 +0900

    update temp_for_crontab.sh

--- temp_for_crontab.sh
+++ temp_for_crontab.sh
@@ -9,3 +9,9 @@ sudo chown pi:pi rpi_cpu_temp
 sudo chmod 775 rpi_cpu_temp
 sudo cp rpi_cpu_temp /usr/local/bin
 cat <(crontab -l) <(echo "*/5 * * * * /bin/bash rpi_cpu_temp >> /var/log/cpu_temp 2>&1") | crontab -
+
+sudo touch /var/log/cpu_temp
+sudo chown pi:pi /var/log/cpu_temp
+
+sudo touch /var/log/gpu_temp
+sudo chown pi:pi /var/log/gpu_temp
