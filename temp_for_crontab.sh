f3ca66c984456c0c4a004fa60b7f6650604ffae1
ipmstyle <ipmstyle@gmail.com>
2021-07-23 15:42:11 +0900

    udpate temp_for_crontab.sh

--- temp_for_crontab.sh
+++ temp_for_crontab.sh
@@ -3,12 +3,12 @@
 sudo chown pi:pi rpi_gpu_temp
 sudo chmod 775 rpi_gpu_temp
 sudo cp rpi_gpu_temp /usr/local/bin
-cat <(crontab -l) <(echo "*/5 * * * * /bin/bash rpi_gpu_temp >> /var/log/gpu_temp 2>&1") | crontab -
+cat <(crontab -l) <(echo "*/5 * * * * /usr/local/bin/rpi_gpu_temp >> /var/log/gpu_temp 2>&1") | crontab -
 
 sudo chown pi:pi rpi_cpu_temp
 sudo chmod 775 rpi_cpu_temp
 sudo cp rpi_cpu_temp /usr/local/bin
-cat <(crontab -l) <(echo "*/5 * * * * /bin/bash rpi_cpu_temp >> /var/log/cpu_temp 2>&1") | crontab -
+cat <(crontab -l) <(echo "*/5 * * * * /usr/local/bin/rpi_cpu_temp >> /var/log/cpu_temp 2>&1") | crontab -
 
 sudo touch /var/log/cpu_temp
 sudo chown pi:pi /var/log/cpu_temp
