694df34a09897b5be78da405151b9505a5718978
ipmstyle <ipmstyle@gmail.com>
2021-07-25 14:29:48 +0000

    Use the current user, do not use a fixed user

--- temp_for_crontab.sh
+++ temp_for_crontab.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-sudo chown pi:pi rpi_gpu_temp
+USER=who | awk '{ print $1}'
+echo $USER
+
+sudo chown $USER:$USER rpi_gpu_temp
 sudo chmod 775 rpi_gpu_temp
 sudo cp rpi_gpu_temp /usr/local/bin
 cat <(crontab -l) <(echo "*/5 * * * * /usr/local/bin/rpi_gpu_temp >> /var/log/gpu_temp 2>&1") | crontab -
 
-sudo chown pi:pi rpi_cpu_temp
+sudo chown $USER:$USER rpi_cpu_temp
 sudo chmod 775 rpi_cpu_temp
 sudo cp rpi_cpu_temp /usr/local/bin
 cat <(crontab -l) <(echo "*/5 * * * * /usr/local/bin/rpi_cpu_temp >> /var/log/cpu_temp 2>&1") | crontab -
 
 sudo touch /var/log/cpu_temp
-sudo chown pi:pi /var/log/cpu_temp
+sudo chown $USER:$USER /var/log/cpu_temp
 
 sudo touch /var/log/gpu_temp
-sudo chown pi:pi /var/log/gpu_temp
+sudo chown $USER:$USER /var/log/gpu_temp
